ab528b6c7a12c958589940483cf0aed811d05eb0
rajeshkanna24 <rajeshkanna2406gmail.com>
2023-07-29 09:15:11 +0530

    terraform

--- 04-data-source/data.tf
+++ 04-data-source/data.tf
@@ -11,3 +11,26 @@ data  "aws_ec2_spot_price"  "example"  {
 output  "price" {
   value = data.aws_ec2_spot_price.example.spot_price
 }
+data  "aws_security_group"  "selected"  {
+  value = data.aws_security_group.selected.id
+}
+
+data  "aws_security_group"  "selected"  {
+  name  = "allow-all"
+}
+output  "sgid"  {
+  value = data.aws_security_group.selected.id
+}
+
+data  "aws_ami" "example" {
+  owners  = ["amazon"]
+  most_recent = true
+  name_regex  = "centos-8-DevOps-practice"
+}
+
+output  "ami" {
+  value = data.aws_ami.example.id
+}
+provider  "aws" {
+  region  = "us-east-1a"
+}
